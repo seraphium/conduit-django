a9b9205882fcf3dbdccdc1400f4c6ea09df8a299
Jackie Zhang <viperking@163.com>
2017-04-16 21:26:36 +0800

    first comment on backend tutorial

---
+++ conduit/apps/authentication/models.py
@@ -0,0 +1,9 @@
+import jwt
+from datetime import datetime, timedelta
+
+from django.conf import settings
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+from django.db import models
+

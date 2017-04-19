74dc95d71b539b0b278a2b7b240d6ddfebf47ceb
seraphium <viperking@163.com>
2017-04-19 17:36:15 +0800

    finished Comment part

--- conduit/apps/articles/models.py
+++ conduit/apps/articles/models.py
@@ -13,3 +13,10 @@ class Article(TimestampModel):
     def __str__(self):
         return self.title
 
+
+class Comment(TimestampModel):
+    body = models.TextField()
+
+    article = models.ForeignKey('articles.Article', related_name='comments', on_delete=models.CASCADE)
+
+    author = models.ForeignKey('profiles.Profile', related_name='comments', on_delete=models.CASCADE)
--- conduit/apps/articles/renderers.py
+++ conduit/apps/articles/renderers.py
@@ -5,3 +5,7 @@ class ArticleJSONRenderer(ConduitJSONRenderer):
     object_label = 'article'
     object_label_plural = 'articles'
 
+
+class CommentJSONRenderer(ConduitJSONRenderer):
+    object_label = 'comment'
+    object_label_plural = 'comments'

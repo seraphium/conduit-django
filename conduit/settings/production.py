ab7b05865de67824c830a7ec7b62fd40e29d89dc
seraphium <viperking@163.com>
2017-05-10 17:50:50 +0800

    added database_url.config installed gunicorn as wsgi server

--- conduit/settings/production.py
+++ conduit/settings/production.py
@@ -1,8 +1,13 @@
 from conduit.settings.common import *
+import dj_database_url
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 

376a4cbb9cb31dbebf56f91534b64f59fb91c785
Jackie Zhang <viperking@163.com>
2017-05-29 15:59:22 +0800

    changed settings

--- conduit/settings/common.py
+++ conduit/settings/common.py
@@ -141,15 +141,15 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__ + '../'))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+#STATICFILES_DIRS = (
+#    'static',
+#)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/

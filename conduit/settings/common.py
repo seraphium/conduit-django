184b177ff1a050c1d5809432a4542b9e3d64443a
Jackie Zhang <viperking@163.com>
2017-05-29 15:52:04 +0800

    add support for static files for heroku’

--- conduit/settings/common.py
+++ conduit/settings/common.py
@@ -124,11 +124,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
 
 AUTH_USER_MODEL = 'authentication.User'
 
@@ -141,3 +136,22 @@ REST_FRAMEWORK = {
         'conduit.apps.authentication.backends.JWTAuthentication',
     ),
 }
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
